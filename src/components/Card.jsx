836b9583f9c88106e1298cc08093348513d8c1d9
Efraín <efrain.arenas@kuantik.mx>
2025-03-22 22:12:48 -0600

    Zoom in when hover and single file plugin

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const ChartContainer = styled.div`
-  /* width: 80%;
+	/* width: 80%;
   max-width: 600px; */
-  display: flex;
-  justify-content: center;
-    align-items: center;
-  height: 100%;
-  background-color: ${({ theme }) => theme.background};
-  padding: 20px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  padding: 20px;
-  border-radius: 10px;
-  margin-bottom: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100%;
+	background-color: ${({ theme }) => theme.background};
+	padding: 20px;
+	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+	padding: 20px;
+	border-radius: 10px;
+	margin-bottom: 20px;
+	transition: all 0.3s ease-in-out;
+
+	&:hover {
+		transform: scale(1.05);
+	}
 `;
 
 export const Card = ({ children }) => {
-  return <ChartContainer>{children}</ChartContainer>;
+	return <ChartContainer>{children}</ChartContainer>;
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import {viteSingleFile} from 'vite-plugin-singlefile';
+
+export default defineConfig({
+  plugins: [react(), viteSingleFile()],
+  /* build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000, // Permite inlining de assets grandes
+    chunkSizeWarningLimit: 100000000,
+  }, */
+});
+
+
+
+
+
+
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+}) */
